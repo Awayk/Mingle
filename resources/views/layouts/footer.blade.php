3c733cdbf5c1930bf2de72eecfd7c8413eba5589
LordRob96 <robin.walter.96@gmail.com>
2018-05-10 14:03:27 +0200

    Added a footer
    Added a footer with corresponding design to the scrolled navbar

--- resources/views/layouts/footer.blade.php
+++ resources/views/layouts/footer.blade.php
@@ -1,44 +1,22 @@
 <footer id="footer">
     <div class="container">
         <div class="row">
-            <div class="col-6 col-sm-3">
-                <h6 class="text-uppercase">More</h6>
-                <ul class="list-unstyled">
-                    <li><a href="//theme.cards" title="Free themes">Theme.cards</a></li>
-                    <li><a href="//www.bootply.com" title="Bootstrap Editor">Bootply</a></li>
-                    <li><a href="//www.codeply.com" title="Frontend Editor Online">Codeply</a></li>
-                    <li><a href="//www.wdstack.com" title="Best Developer Designer Resources">WDStack</a></li>
-                </ul>
-            </div>
-            <div class="col-6 col-sm-3 column">
+            <div class="col-6 col-sm-4 column">
                 <h6 class="text-uppercase">About</h6>
                 <ul class="list-unstyled">
-                    <li><a href="#" data-toggle="modal" data-target="#alertModal">Contact Us</a></li>
-                    <li><a href="#" data-toggle="modal" data-target="#alertModal">Delivery Information</a></li>
-                    <li><a href="#" data-toggle="modal" data-target="#alertModal">Privacy Policy</a></li>
-                    <li><a href="#" data-toggle="modal" data-target="#alertModal">Terms &amp; Conditions</a></li>
+                    <li><a href="#">Contact Us</a></li>
+                    <li><a href="#">Delivery Information</a></li>
+                    <li><a href="#">Privacy Policy</a></li>
+                    <li><a href="#">Terms &amp; Conditions</a></li>
                 </ul>
             </div>
-            <div class="col-12 col-sm-3 column">
-                <h6 class="text-uppercase">Stay Posted</h6>
-                <form>
-                    <div class="form-group">
-                        <input type="text" class="form-control" title="No spam, we promise!" placeholder="Tell us your email">
-                    </div>
-                    <div class="form-group">
-                        <button class="btn btn-primary" data-toggle="modal" data-target="#alertModal" type="button">Subscribe for updates</button>
-                    </div>
-                </form>
-            </div>
-            <div class="col-12 col-sm-3 text-right">
+            <div class="col-12 col-sm-4">
                 <h6 class="text-uppercase">Follow</h6>
-                <ul class="list-inline">
-                    <li class="list-inline-item"><a rel="nofollow" href="//twitter.com/bootstrap4guide" title="Twitter"><i class="icon-lg ion-social-twitter-outline"></i></a>&nbsp;</li>
-                    <li class="list-inline-item"><a rel="nofollow" href="//twitter.com/bootstrap4guide" title="Facebook"><i class="icon-lg ion-social-facebook-outline"></i></a></li>
-                </ul>
+                <a href="https://twitter.com/LonelinessEU?ref_src=twsrc%5Etfw" class="twitter-follow-button" data-show-count="false">Follow @LonelinessEU</a><script async src="https://platform.twitter.com/widgets.js" charset="utf-8"></script>
+            </div>
+            <div class="col-12 col-sm-4">
+              <img src="{{asset('/img/Mlogo.png')}}" class="float-right" alt="">
             </div>
         </div>
-        <br>
-        <span class="pull-right text-muted small"><a href="http://themes.guide">Made by Themes.guide</a> ©2015-2017</span>
-    </div>
+        </div>
 </footer>
