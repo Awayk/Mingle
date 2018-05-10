9e013af63c99430f763dd15cb48c19543f4ac7b2
Awayk <fyn-robin@hotmail.de>
2018-05-10 14:08:17 +0200

    Layout Changes & Addition of Footer

--- public/js/script.js
+++ public/js/script.js
@@ -1,8 +1,13 @@
+
+// JavaScript (JQuery) Code for Navbar Changes on scroll
+
 $(function () {
   $(document).scroll(function () {
 	  var $nav = $("#navBar");
 	  $nav.toggleClass('scrolled', $(this).scrollTop() > $nav.height());
     var $navOuter = $("#outerNavbar");
     $navOuter.toggleClass('scrolled', $(this).scrollTop() > $navOuter.height());
+    var $search = $("#searchButton");
+    $search.toggleClass('scrolled', $(this).scrollTop() > $search.height());
 	});
 });
