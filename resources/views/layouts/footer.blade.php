e4877b8464d76a34fdd9700dd86deeee8a720f63
LordRob96 <robin.walter.96@gmail.com>
2018-05-10 15:31:23 +0200

    changed toolbar in framework to Fyn's Version

--- resources/views/layouts/footer.blade.php
+++ resources/views/layouts/footer.blade.php
@@ -1,22 +1,51 @@
-<footer id="footer">
-    <div class="container">
-        <div class="row">
-            <div class="col-6 col-sm-4 column">
-                <h6 class="text-uppercase">About</h6>
-                <ul class="list-unstyled">
-                    <li><a href="#">Contact Us</a></li>
-                    <li><a href="#">Delivery Information</a></li>
-                    <li><a href="#">Privacy Policy</a></li>
-                    <li><a href="#">Terms &amp; Conditions</a></li>
-                </ul>
-            </div>
-            <div class="col-12 col-sm-4">
-                <h6 class="text-uppercase">Follow</h6>
-                <a href="https://twitter.com/LonelinessEU?ref_src=twsrc%5Etfw" class="twitter-follow-button" data-show-count="false">Follow @LonelinessEU</a><script async src="https://platform.twitter.com/widgets.js" charset="utf-8"></script>
-            </div>
-            <div class="col-12 col-sm-4">
-              <img src="{{asset('/img/Mlogo.png')}}" class="float-right" alt="">
-            </div>
+<!-- Last Section - Footer -->
+<section id="section5">
+  <div class="container-fluid mt-3">
+    <div class="row justify-content-md-center">
+      <div class="col-sm-3">
+        <ul>
+          <h6>Support</h6>
+          <hr id="footerDivider">
+          <a href="#" id="footerLink">Chat</a><br>
+          <a href="#" id="footerLink">Contact</a><br>
+          <a href="#" id="footerLink">FAQ</a><br>
+          <a href="#" id="footerLink">Forum</a>
+        </ul>
+      </div>
+      <div class="col-sm-3">
+        <ul>
+          <h6>About</h6>
+          <hr id="footerDivider">
+          <a href="#" id="footerLink">Impressum</a><br>
+          <a href="#" id="footerLink">Directions</a><br>
+          <a href="#" id="footerLink">Legal</a>
+        </ul>
+      </div>
+    <div class="col-sm-3">
+      <!-- <ul> -->
+      <!-- <h6>Social Media</h6> -->
+      <!-- <hr id="footerDivider"> -->
+      <div class="row">
+        <div class="col-md-2">
+          <span class="fab fa-twitter fa-3x"></span>
         </div>
+        <div class="col-md-2">
+          <span class="fab fa-facebook fa-3x"></span>
         </div>
-</footer>
+        <div class="col-md-2">
+          <span class="fab fa-instagram fa-3x"></span>
+        </div>
+        <div class="col-md-2">
+          <span class="fab fa-reddit-square fa-3x"></span>
+        </div>
+      </div>
+      <!-- <a href="#" class="mb-3" id="footerLink"><span class="fab fa-twitter fa-lg mr-2" id="twitterLink"></span>Twitter</a><br> -->
+      <!-- <a href="#" class="mb-3" id="footerLink"><span class="fab fa-facebook fa-lg mr-2" id="facebookLink"></span>Facebook</a><br> -->
+      <!-- <a href="#" class="mb-3" id="footerLink"><span class="fab fa-instagram fa-lg mr-2" id="instagramLink"></span>Instagram</a><br> -->
+      <!-- <a href="#" class="mb-3" id="footerLink"><span class="fab fa-reddit-square fa-lg mr-2" id="redditLink"></span>Reddit</a> -->
+      <!-- </ul> -->
+    </div>
+  </div>
+
+  </div>
+</section>
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -7,9 +7,10 @@
 
 @section('content')
 
-  <!-- Top Section - MingleNet -->
-  <section id="section1">
-    <img class="d-block w-100" src="/img/NetworkFitted.jpg" alt="Network" id="homeImage">
+
+    <!-- Top Section - Promotion of MingleNet & Possibility to join Network -->
+    <section id="section1">
+      <img class="d-block w-100" src="/img/NetworkFitted.jpg" alt="Network" id="homeImage">
       <div class="carousel-caption">
         <div class="container" id="captionContainer">
           <h1 class="display-4">Join MingleNet</h1>
@@ -17,68 +18,71 @@
           <button class="btn btn-light btn-outline-light" type="button" name="button">Become Part of it!</button>
         </div>
       </div>
-  </section>
+    </section>
 
-  <!-- Informative Section -->
-  <section id="section2">
-    <div class="container my-5">
-      <div class="row">
-        <div class="col-md-7 align-self-center" id="textualColumn">
-          <h4 class="mb-4">Collaboration is Necessary - Now More Than Ever</h4>
-          <p>In a world subject to rapid changes with thousands of possibilities<br>for people to connect, loneliness is a greater problem than suspected.</p>
-          <p>We as organizations and communities need to work together closely<br>in order to assist in matters like this.</p>
-        </div>
-        <div class="col-md-5">
-          <img class="d-block w-100 rounded" src="/img/Globe.jpg" alt="Globe">
-        </div>
-      </div>
-    </div>
-    </div>
-    </div>
-  </section>
-
-
-  <!-- Middle Section - Cards against Loneliness -->
-  <section id="section3">
-    <div class="container">
-      <h1 id="section2Header" class="text-center pt-3">Headline</h1>
-      <div class="card-deck">
-        <div class="card rounded">
-          <img class="card-image-top w-100 d-block rounded-top" src="/img/Waterglass.jpg" alt="">
-          <div class="card-body">
-            <h4 class="card-title">Providing a Platform</h4>
-            <p class="card-text"> Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute
-              irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.</p>
-            <div class="card-footer">
-              <small class="text-muted">Further Information: ...</small>
-            </div>
+    <!-- Informative Section - Text & Picture -->
+    <section id="section2">
+      <div class="container py-5">
+        <div class="row">
+          <div class="col-md-7 align-self-center" id="textualColumn">
+            <h4 class="mb-4">Collaboration is Necessary - Now More Than Ever</h4>
+            <p>In a world subject to rapid changes with thousands of possibilities<br>for people to connect, loneliness is a greater problem than suspected.</p>
+            <p>We as organizations and communities need to work together closely<br>in order to assist in matters like this.</p>
           </div>
-        </div>
-        <div class="card rounded">
-          <img class="card-image-top w-100 d-block rounded-top" src="/img/StayStrong.jpg" alt="">
-          <div class="card-body">
-            <h4 class="card-title">Connecting Individuals</h4>
-            <p class="card-text"> Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute
-              irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.</p>
-            <div class="card-footer">
-              <small class="text-muted">Further Information: ...</small>
-            </div>
-          </div>
-        </div>
-        <div class="card rounded">
-          <img class="card-image-top w-100 d-block rounded-top" src="/img/Globe.jpg" alt="">
-          <div class="card-body">
-            <h4 class="card-title">Informing People</h4>
-            <p class="card-text"> Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute
-              irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.</p>
-            <div class="card-footer">
-              <small class="text-muted">Further Information: ...</small>
-            </div>
+          <div class="col-md-5">
+            <img class="d-block w-100 rounded" src="/img/Globe.jpg" alt="Globe">
           </div>
         </div>
       </div>
-    </div>
-  </section>
+    </section>
+
+    <!-- Informative Section 2 - Text & Picture-->
+    <section id="section3">
+      <div class="container py-5">
+        <div class="row">
+          <div class="col-md-5">
+            <img class="d-block w-100 rounded" src="/img/LongRoad.jpg" alt="Office Desk">
+          </div>
+          <div class="col-md-7 pl-5 align-self-center" id="textualColumn">
+            <h4 class="mb-4">A Long Road - But We are Prepared to Take it on</h4>
+            <p>In a world subject to rapid changes with thousands of possibilities<br>for people to connect, loneliness is a greater problem than suspected.</p>
+            <p>We as organizations and communities need to work together closely<br>in order to assist in matters like this.</p>
+          </div>
+        </div>
+      </div>
+    </section>
+
+    <!-- Lower Section - Info Cards -->
+    <section id="section4">
+      <div class="container">
+        <div class="card-deck py-5">
+          <div class="card rounded">
+            <img class="card-image-top img-fluid w-100 rounded-top" src="/img/Waterglass.jpg" alt="">
+            <div class="card-body text-center">
+              <h5 class="card-title">Providing a Platform</h5>
+              <p class="card-text"> Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+              <button id="readButton" class="btn btn-sm btn-disabled mb-2" type="button">Read more</button>
+            </div>
+          </div>
+          <div class="card rounded">
+            <img class="card-image-top img-fluid h-50 w-100 rounded-top" src="/img/StayStrong.jpg" alt="">
+            <div class="card-body text-center">
+              <h5 class="card-title">Connecting Individuals</h5>
+              <p class="card-text"> Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+              <button id="readButton" class="btn btn-sm btn-disabled mb-2" type="button">Read more</button>
+            </div>
+          </div>
+          <div class="card rounded">
+            <img class="card-image-top img-fluid w-100 rounded-top" src="/img/Globe.jpg" alt="">
+            <div class="card-body text-center">
+              <h5 class="card-title">Informing People</h5>
+              <p class="card-text"> Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+              <button id="readButton" class="btn btn-sm btn-disabled mb-2" type="button">Read more</button>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
 
 
 @endsection
