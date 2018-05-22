0c866923a1c4cd6b32fa0d5eecd05f532aca3874
Awayk <fyn-robin@hotmail.de>
2018-05-22 09:33:19 +0200

    Name Changes, Image Changes & Creation "MingleNet-Page"

--- resources/views/layouts/footer.blade.php
+++ resources/views/layouts/footer.blade.php
@@ -1,7 +1,7 @@
 <!-- Footer Section:  -->
 <section id="footerSection">
-  <div class="container-fluid mt-3">
-    <div class="row justify-content-md-center">
+  <div class="container mt-3">
+    <div class="row justify-content-md-start">
       <div class="col-sm-3">
         <ul>
           <h6 id="footerHeader">Support</h6>
@@ -22,30 +22,29 @@
         </ul>
       </div>
       <div class="col-sm-3">
-        <!-- <ul> -->
-        <!-- <h6>Social Media</h6> -->
-        <!-- <hr id="footerDivider"> -->
-        <div class="d-flex flex-row justify-content-end my-3">
-          <div class="col-md-2 mr-1">
-            <a href="#" id="twitterLink"><span class="fab fa-twitter fa-3x"></span></a>
-          </div>
-          <div class="col-md-2 ml-1">
-            <a href="#" id="facebookLink"><span class="fab fa-facebook fa-3x"></span></a>
-          </div>
-        </div>
-        <div class="d-flex flex-row justify-content-end my-2">
-          <div class="col-md-2 mr-1">
-            <a href="#" id="instagramLink"><span class="fab fa-instagram fa-3x"></span></a>
-          </div>
-          <div class="col-md-2 ml-1">
-            <a href="#" id="redditLink"><span class="fab fa-reddit-square fa-3x"></span></a>
-          </div>
-        </div>
-        <!-- <a href="#" class="mb-3" id="footerLink"><span class="fab fa-twitter fa-lg mr-2" id="twitterLink"></span>Twitter</a><br> -->
-        <!-- <a href="#" class="mb-3" id="footerLink"><span class="fab fa-facebook fa-lg mr-2" id="facebookLink"></span>Facebook</a><br> -->
-        <!-- <a href="#" class="mb-3" id="footerLink"><span class="fab fa-instagram fa-lg mr-2" id="instagramLink"></span>Instagram</a><br> -->
-        <!-- <a href="#" class="mb-3" id="footerLink"><span class="fab fa-reddit-square fa-lg mr-2" id="redditLink"></span>Reddit</a> -->
-        <!-- </ul> -->
+        <ul>
+          <h6 id="footerHeader">Third Footer List</h6>
+          <hr id="footerDivider">
+          <a href="#" id="footerLink">?</a><br>
+          <a href="#" id="footerLink">?</a><br>
+          <a href="#" id="footerLink">?</a>
+        </ul>
+      </div>
+      <div class="col-sm-3">
+        <ul>
+          <h6>Social Media</h6>
+          <hr id="footerDivider">
+          <a href="#" class="mb-3" id="footerLink"><span class="fab fa-twitter fa-lg mr-2" id="twitterLink"></span>Twitter</a><br>
+          <a href="#" class="mb-3" id="footerLink"><span class="fab fa-facebook fa-lg mr-2" id="facebookLink"></span>Facebook</a><br>
+          <a href="#" class="mb-3" id="footerLink"><span class="fab fa-instagram fa-lg mr-2" id="instagramLink"></span>Instagram</a><br>
+          <a href="#" class="mb-3" id="footerLink"><span class="fab fa-reddit-square fa-lg mr-2" id="redditLink"></span>Reddit</a>
+        </ul>
+      </div>
+    </div>
+    <div class="row">
+      <div class="col-4">
+
+        <small>© 2018 Mingle, All Rights Reserved.</small>
       </div>
     </div>
 
